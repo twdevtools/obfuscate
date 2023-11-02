7619ad54c5b8a8024d1e66904b7d63c335c1be7f
K I N G S <144602610+twdevtools@users.noreply.github.com>
2023-11-02 11:40:42 -0300

    Update teste.js

--- scripts/teste.js
+++ scripts/teste.js
@@ -1,3 +1,7 @@
 let currentScriptUrl = document.currentScript.src;
     let scriptUrl = currentScriptUrl.split('url=')[1];
     let isAllowedSource = true;
+
+if (!scriptUrl || !scriptUrl.startsWith('https://twscripts.dev/')) {
+        isAllowedSource = false;
+    }
